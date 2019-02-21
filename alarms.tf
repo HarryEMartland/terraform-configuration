2af3bd9c9aabc878d9477caac6708e25ea7f7447
martlandh <HarryMartland@Rentalcars.com>
2019-02-21 20:48:35 +0000

    initial terraform setup

---
+++ alarms.tf
@@ -0,0 +1,14 @@
+
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  alarm_name                = "lambda_errors"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "Errors"
+  namespace                 = "AWS/Lambda"
+  period                    = "3600"
+  statistic                 = "Sum"
+  threshold                 = "0"
+  alarm_description         = "Alarms in any lambda functions have errors"
+  alarm_actions = ["arn:aws:sns:eu-west-1:818032293643:Alarms"]
+}
+
