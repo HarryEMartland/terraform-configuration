5f8d3f5256cd9bdcd077594ff0dcc4421a38daab
martlandh <HarryMartland@Rentalcars.com>
2019-02-21 21:10:18 +0000

    added too many tweets alarm

--- alarms.tf
+++ alarms.tf
@@ -12,3 +12,15 @@ resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   alarm_actions = ["arn:aws:sns:eu-west-1:818032293643:Alarms"]
 }
 
+resource "aws_cloudwatch_metric_alarm" "too_many_tweets" {
+  alarm_name                = "Too Many Tweets to Proccess"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "foundTweets"
+  namespace                 = "LogMetrics"
+  period                    = "3600"
+  statistic                 = "Max"
+  threshold                 = "100"
+  alarm_description         = "If there are too many tweets the function will miss some"
+  alarm_actions = ["arn:aws:sns:eu-west-1:818032293643:Alarms"]
+}
