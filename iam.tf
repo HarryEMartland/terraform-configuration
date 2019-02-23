c61458cd81ab04abae78b1e4e01d918e2150f409
martlandh <HarryMartland@Rentalcars.com>
2019-02-23 20:28:34 +0000

    Added users and permissions for new lambda deploy users

--- iam.tf
+++ iam.tf
@@ -1,4 +1,9 @@
 
+variable "lambdas" {
+  type = "list"
+  default = ["LearnToCodesTwitterBot","HarryBotRetweet"]
+}
+
 resource "aws_iam_user" "read_only" {
   name = "read_only"
 }
@@ -6,4 +11,35 @@ resource "aws_iam_user" "read_only" {
 resource "aws_iam_user_policy_attachment" "test-attach" {
   user       = "${aws_iam_user.read_only.name}"
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_user" "lambda-deploys" {
+  name = "lambda-peploy-${var.lambdas[count.index]}"
+  count = "${length(var.lambdas)}"
+}
+
+resource "aws_iam_user_policy" "lambda-deploys" {
+  name = "lambda-deploy-${var.lambdas[count.index]}"
+  user = "${aws_iam_user.lambda-deploys.*.name[count.index]}"
+  count = "${length(var.lambdas)}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "lambda:UpdateFunctionCode",
+        "lambda:GetFunction"
+      ],
+      "Effect": "Allow",
+      "Resource": ["arn:aws:lambda:eu-west-1:818032293643:function:${var.lambdas[count.index]}"]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_access_key" "lambda-deploys" {
+  user = "lambda-peploy-${var.lambdas[count.index]}"
+  count = "${length(var.lambdas)}"
 }
