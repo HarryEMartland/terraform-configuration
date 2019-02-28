fc069e9a7643246c4ce3178ddbf901eb0aa9057d
martlandh <HarryMartland@Rentalcars.com>
2019-02-28 20:56:29 +0000

    set storage class

--- s3.tf
+++ s3.tf
@@ -2,9 +2,11 @@
 resource "aws_s3_bucket" "music" {
   bucket = "harry-martland-music"
   acl    = "private"
+  storage_class = "STANDARD_IA"
 }
 
 resource "aws_s3_bucket" "backup" {
   bucket = "harry-martland-backup"
   acl    = "private"
+  storage_class = "GLACIER"
 }
