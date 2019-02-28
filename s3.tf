432165ffb813e6ffa9bfe49411e009af558c3f6f
martlandh <HarryMartland@Rentalcars.com>
2019-02-28 20:42:18 +0000

    changed bucket names

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
 
 resource "aws_s3_bucket" "music" {
-  bucket = "hm-music"
+  bucket = "harry-martland-music"
   acl    = "private"
 }
 
 resource "aws_s3_bucket" "backup" {
-  bucket = "hm-backup"
+  bucket = "harry-martland-backup"
   acl    = "private"
 }
