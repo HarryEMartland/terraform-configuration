403e0ea6106aafe1504ad39fe583bccd2237b01d
martlandh <HarryMartland@Rentalcars.com>
2019-03-02 19:33:21 +0000

    removed incorrect storage class

--- s3.tf
+++ s3.tf
@@ -2,11 +2,9 @@
 resource "aws_s3_bucket" "music" {
   bucket = "harry-martland-music"
   acl    = "private"
-  storage_class = "STANDARD_IA"
 }
 
 resource "aws_s3_bucket" "backup" {
   bucket = "harry-martland-backup"
   acl    = "private"
-  storage_class = "GLACIER"
 }
