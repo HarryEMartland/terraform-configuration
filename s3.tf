a5892374e5cafc7c3fe89e6b4df743df382fc3c8
martlandh <HarryMartland@Rentalcars.com>
2019-03-05 23:21:00 +0000

    changed to just put events for music bucket

--- s3.tf
+++ s3.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
   lambda_function {
     lambda_function_arn = "arn:aws:lambda:eu-west-1:818032293643:function:S3Flack2Mp3"
-    events              = ["s3:ObjectCreated:*"]
+    events              = ["s3:ObjectCreated:Put"]
     filter_suffix       = ".flac"
   }
 }
