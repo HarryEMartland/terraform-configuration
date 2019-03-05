f5e3fffb8c7026d5e9c40a90fbac873aa749675e
martlandh <HarryMartland@Rentalcars.com>
2019-03-05 22:29:55 +0000

    emit put events from music bucket

--- s3.tf
+++ s3.tf
@@ -8,3 +8,13 @@ resource "aws_s3_bucket" "backup" {
   bucket = "harry-martland-backup"
   acl    = "private"
 }
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = "${aws_s3_bucket.music.id}"
+
+  lambda_function {
+    lambda_function_arn = "arn:aws:lambda:eu-west-1:818032293643:function:S3Flack2Mp3"
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".flac"
+  }
+}
