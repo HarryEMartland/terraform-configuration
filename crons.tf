e2e31211b47a26a3ac0ed5423a2fa54ac3bd1059
martlandh <HarryMartland@Rentalcars.com>
2019-03-06 19:13:15 +0000

    fixed cron expression

--- crons.tf
+++ crons.tf
@@ -2,5 +2,5 @@
 resource "aws_cloudwatch_event_rule" "every-half-hour" {
   name        = "EveryHalfHour"
   description = "Runs every half hour at 36 and 06"
-  schedule_expression = "6,36 * ? * * *"
+  schedule_expression = "cron(6,36 * ? * * *)"
 }
