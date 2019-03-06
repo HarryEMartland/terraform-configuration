da6b449cb596300c2116924c8d0ceb077abdb3fb
martlandh <HarryMartland@Rentalcars.com>
2019-03-06 19:03:31 +0000

    added half hour schedule

---
+++ crons.tf
@@ -0,0 +1,6 @@
+
+resource "aws_cloudwatch_event_rule" "every-half-hour" {
+  name        = "EveryHalfHour"
+  description = "Runs every half hour at 36 and 06"
+  schedule_expression = "6,36 * * * *"
+}
